bc83c26f9942a97754bec8210da23605d7bf3cae
MatiasCardoso000 <cardosomatias.000@gmail.com>
2024-10-11 11:49:20 -0300

    Se crearon funciones para obtener los pokemons. Una función para desglosar el array de stats y mostrarlos en pantalla, los mismo con el de las habilidades.

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
-function getPokemon() {
-  const response = fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-  response.then((data) => data.json().then((data) => console.log(data)));
+const pokemon_name = document.querySelector(".pokemon-name");
+const pokemon_id = document.querySelector(".pokemon-id");
+const pokemon_abilities = document.querySelector(".pokemon-abilities");
+const pokemon_stats = document.querySelector(".pokemon-stats");
+
+/* ######################################## */
+const inputSearch = document.getElementById("poke-name");
+const form = document.querySelector("form");
+/* ######################################## */
+let pokemon = undefined;
+/* ######################################## */
+inputSearch.addEventListener("input", (e) => {
+  const value = e.target.value;
+  pokemon = value;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getPokemon();
+});
+/* ######################################## */
+async function getPokemon() {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon.toLocaleLowerCase()}`;
+  const response = await fetch(URL);
+  response.json().then((data) => {
+    showPokemonInfo(data);
+    console.log(data);
+  });
 }
 
-getPokemon();
+function showPokemonInfo(data) {
+  const { name, id, abilities, stats } = data;
+  getPokemonStats(stats);
+  getPokemonAbilities(abilities);
+  pokemon_name.textContent = name;
+  pokemon_id.textContent = id;
+}
+
+function getPokemonStats(stats) {
+  stats.forEach((stat, i) => {
+    const li = document.createElement("li");
+    li.textContent = `${stat.base_stat} ${stat.stat.name}`;
+    pokemon_stats.append(li);
+  });
+}
+
+function getPokemonAbilities(abilities) {
+  abilities.forEach((ability, i) => {
+    const li = document.createElement("li");
+    li.textContent = `${ability.ability} `;
+    pokemon_stats.append(li);
+  });
+  console.log(abilities);
+}
