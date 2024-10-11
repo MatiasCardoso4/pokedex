5fce612102197c45c9c9e8d6fa8bafcbc5616e29
MatiasCardoso000 <cardosomatias.000@gmail.com>
2024-10-11 15:02:52 -0300

    Ahora al buscar un pokemon aparece su imagen , se agregaron nuevos estilos a los inputs y boton y a las estadisticas de los pokemon

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const pokemon_name = document.querySelector(".pokemon-name");
+const pokemon_sprite = document.querySelector(".pokemon-sprite");
 const pokemon_id = document.querySelector(".pokemon-id");
 const pokemon_abilities = document.querySelector(".pokemon-abilities");
 const pokemon_stats = document.querySelector(".pokemon-stats");
 const pokemon_type = document.querySelector(".pokemon-type");
+
 /* ######################################## */
 const inputSearch = document.getElementById("poke-name");
 const form = document.querySelector("form");
@@ -24,24 +26,38 @@ async function getPokemon() {
   const response = await fetch(URL);
   response.json().then((data) => {
     showPokemonInfo(data);
+    // console.log(data);
   });
 }
 
 function showPokemonInfo(data) {
   cleanDisplay();
-  const { name, id, abilities, stats, types } = data;
+
+  const { name, sprites, id, abilities, stats, types } = data;
   pokemon_name.textContent = name;
-  pokemon_id.textContent = id;
+  data ? (pokemon_sprite.style.display = "block") : "none";
+  pokemon_sprite.src = `${sprites.front_default}`;
+  pokemon_id.textContent = `id: ${id}`;
   getPokemonStats(stats);
-  getPokemonAbilities(abilities);
-  getPokemonTypes(types);
+  // getPokemonAbilities(abilities);
+  // getPokemonTypes(types);
 }
 
 function getPokemonStats(stats) {
+  const stats_container = document.querySelector(".pokemon-stats-container");
+  const h5 = document.createElement("h5");
+  h5.textContent = "Stats";
   stats.forEach((stat) => {
     const li = document.createElement("li");
-    li.textContent = `${stat.base_stat} ${stat.stat.name}`;
+    const stat_value_paragraph = document.createElement("p");
+    const stat_name_paragraph = document.createElement("p");
+
+    stat_value_paragraph.append(`${stat.base_stat}`);
+    stat_name_paragraph.append(`${stat.stat.name}`);
+    li.append(stat_value_paragraph, stat_name_paragraph);
     pokemon_stats.append(li);
+
+    stats_container.append(h5, pokemon_stats);
   });
 }
 
@@ -64,7 +80,7 @@ function getPokemonTypes(types) {
 function cleanDisplay() {
   pokemon_name.textContent = "";
   pokemon_id.textContent = "";
-  pokemon_abilities.textContent = "";
   pokemon_stats.textContent = "";
+  pokemon_abilities.textContent = "";
   pokemon_type.textContent = "";
 }
