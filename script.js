5e8e2102149ddb8d6ac11eb41106d8d4d3ca652b
MatiasCardoso000 <cardosomatias.000@gmail.com>
2024-10-12 17:24:13 -0300

    Al cargar los stats del pokemon el H5 ya no se vuelve a generar al clickear nuevamente en el boton. Se agregaron comentarios de explicación.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ELEMENTOS DE LA POKEDEX DONDE SE VA A MOSTRAR LA INFORMACIÓN DEL POKEMON
 const pokemon_name = document.querySelector(".pokemon-name");
 const pokemon_sprite = document.querySelector(".pokemon-sprite");
 const pokemon_id = document.querySelector(".pokemon-id");
@@ -8,25 +9,34 @@ const pokemon_sprite_container = document.querySelector(
   ".pokemon-sprite-container"
 );
 
+//ERROR MESSAGE
 const error_message = document.querySelector(".error-message");
-/* ######################################## */
+
+// INPUT Y FORMULARIO
 const inputSearch = document.getElementById("poke-name");
 const form = document.querySelector("form");
-/* ######################################## */
+
+//VARIABLE PARA GUARDAR EL NOMBRE DEL POKEMON
 let pokemon = undefined;
-/* ######################################## */
+
+//H5 ENCABEZADO PARA LOS STATS
+const h5 = document.createElement("h5");
+h5.textContent = "Stats";
+
+//INPUT PARA BUSCAR EL POKEMON
 inputSearch.addEventListener("input", (e) => {
   const value = e.target.value;
   pokemon = value;
 });
 
+//FORMULARIO QUE LLAMA A LAS FUNCIONES DE MUESTRA DE ERROR Y PARA OBTENER AL POKEMON
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   showError();
   getPokemon();
 });
 
-/* ######################################## */
+//FUNCIÓN PARA OBTENER LOS POKEMON
 async function getPokemon() {
   try {
     const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon.toLocaleLowerCase()}`;
@@ -40,6 +50,7 @@ async function getPokemon() {
   }
 }
 
+//FUNCIÓN PARA MOSTRAR LOS POKEMON EN PANTALLA
 function showPokemonInfo(data) {
   cleanDisplay();
 
@@ -47,19 +58,21 @@ function showPokemonInfo(data) {
   pokemon_name.textContent = `${name
     .slice(0, 1)
     .toLocaleUpperCase()}${name.slice(1)}`;
+
   data && (error_message.textContent = "");
   data ? (pokemon_sprite.style.display = "block") : "none";
   data ? (pokemon_sprite_container.style.display = "block") : "none";
   pokemon_sprite.src = `${sprites.front_default}`;
+
   getPokemonStats(stats);
   // getPokemonAbilities(abilities);
   // getPokemonTypes(types);
 }
 
+//FUNCIÓN CON UN BUCLE PARA IR MOSTRANDO CADA STAT
 function getPokemonStats(stats) {
   const stats_container = document.querySelector(".pokemon-stats-container");
-  const h5 = document.createElement("h5");
-  h5.textContent = "Stats";
+
   stats.forEach((stat) => {
     const li = document.createElement("li");
     const stat_value_paragraph = document.createElement("p");
@@ -72,12 +85,12 @@ function getPokemonStats(stats) {
       )}`
     );
     li.append(stat_value_paragraph, stat_name_paragraph);
-    pokemon_stats.append(li);
-
+    pokemon_stats.appendChild(li);
     stats_container.append(h5, pokemon_stats);
   });
 }
 
+//FUNCIÓN CON UN BUCLE PARA IR MOSTRANDO CADA HABILIDAD
 function getPokemonAbilities(abilities) {
   abilities.forEach((ability) => {
     const li = document.createElement("li");
@@ -86,6 +99,7 @@ function getPokemonAbilities(abilities) {
   });
 }
 
+//FUNCIÓN CON UN BUCLE PARA IR MOSTRANDO CADA TIPO DE POKEMON
 function getPokemonTypes(types) {
   types.forEach((type) => {
     const li = document.createElement("li");
@@ -94,6 +108,7 @@ function getPokemonTypes(types) {
   });
 }
 
+//FUNCIÓN QUE SIRVE PARA LIMPIAR LA PANTALLA AL BUSCAR UN POKEMON NUEVO
 function cleanDisplay() {
   pokemon_name.textContent = "";
   pokemon_id.textContent = "";
@@ -102,6 +117,7 @@ function cleanDisplay() {
   pokemon_type.textContent = "";
 }
 
+//FUNCIÓN QUE MUESTRA UN MENSAJE DE ERROR EN PANTALLA AL NO INGRESAR UN NOMBRE
 function showError() {
   if (inputSearch.validity.valueMissing) {
     error_message.textContent = "Please type a pokemon name";
